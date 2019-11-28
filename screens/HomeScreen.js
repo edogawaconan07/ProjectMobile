a3009d89f57586ad9d4a4af01e1f654dcb5e4b6d
Huy <huybeep69@gmail.com>
2019-11-28 14:38:32 +0700

    Update file Homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,78 +1,84 @@
-import React, { Component } from 'react'
-import { StyleSheet,Text, View ,Button, Image,Dimensions,ScrollView,ListView } from 'react-native'
-//import Icon from "react-native-vector-icons/MaterialCommunityIcons";
-// import Snapcarousel  from "./SnapCarousel/SnapCarousel"
-// import {ENTRIES1,ENTRIES2} from './entries'
+import React, { Component } from 'react';
+import { 
+  Platform, 
+  StyleSheet, 
+  Text, 
+  View, 
+  FlatList } from 'react-native';
 import CardItem from '../components/CardItem'
-import { NetworkInfo } from "react-native-network-info";
-
-
-var URL="http://localhost/AppMobile/getdata.php"
-export default class HomeScreen extends Component {
-// 	constructor(props){
-// 		super(props);
-// 		this.state = {
-// 			dataSource: new ListView.DataSource({rowHasChanged:(r1,r2)=>r1!==r2})
-// 		}
-// 	}
-	
-// 	fetchData(){
-// 		fetch(URL)
-// 		.then((response)=>response.json())
-// 		.then((responseData)=>{
-// 			this.setState({
-// 				dataSource:this.state.dataSource.cloneWithRows(responseData)
-// 			})
-// 		})
-// 		.done()
-// 	}
-	
-	componentDidMount = async() =>{
-		NetworkInfo.getIPV4Address().then(ipv4Address => {
-			console.log(ipv4Address);
-		  });
-		
-		//this.fetchData();
-	}
-// 	taoHang(property){
-// 		return(
-// 		<View>
-// 			<Text>{property.carid}</Text>
-// 		</View>
-// 		);
-// 	}
+// class FlatListItem extends Component {
+//   render() {
+//     return (
+//       //Định dạng css từng item theo ý.
+//       <View style={styles.flatlist_container}>
+//         <Text style={styles.text}>{this.props.item.XeID}</Text>
+//         <Text style={styles.text}>{this.props.item.Tenxe}</Text>
+//       </View>
+//     );
+//   }
+// }
 
 
 
-	render() {
-		return (
-			<ScrollView style={styles.main}>
-				<View></View>
-				{/* <ListView dataSource={this.state.dataSource} renderRow={this.taoHang}/>				 */}
-			</ScrollView>
-		)
-	}
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //Khai báo array rỗng là myData để chứa dữ liệu load về.
+      myData: [],
+    };
+  }
+
+  loadDataFromServer = () => {
+    //Đổi lại IP cho đúng. có thể đăng ký hosting để dùng khỏi đổi ip tới lui. Gợi ý hosting free 000webhost
+    fetch('https://huynguyen1401.000webhostapp.com/Getdata.php')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          myData: responseJson,
+        });
+      })
+      .catch((e) => {
+        alert(e)
+      });
+  }
+  componentDidMount() {
+    //Tạo hàm loadDataFromServer riêng tí còn dùng nữa khỏi viết lại
+    this.loadDataFromServer();
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.myData}
+          renderItem={({ item, index }) => {
+            return (
+              //Tạo 1 class mới tên FLatListItem có thể đặt tên khác. như trên dòng code 4.
+              <CardItem item={item} index={index}>
+
+              </CardItem>
+            );
+          }}
+          //FlatList phải có key. ở đây ta dùng trường ID làm key.
+          keyExtractor={({XeID}, index) => XeID}
+        >
+        </FlatList>
+      </View>
+    );
+  }
 }
-// const styles = StyleSheet.create({
-// 	main: {
-// 		flex: 1,
-// 		flexDirection: 'column',
-// 		backgroundColor: "#FFFFFF",
-// 	},
-	
-// })
-// const portraitStyles = StyleSheet.create({
-// 		main: {
-// 		flex: 1,
-// 		flexDirection: 'column',
-// 		backgroundColor: "#FFFFFF",
-// 	},
-//    });
-   
-//    const landscapeStyles = StyleSheet.create({
-// 	main: {
-// 		flex: 1,
-// 		flexDirection: 'column',
-// 		backgroundColor: "#FFFFFF",
-// 	},
-//    });
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  flatlist_container: {
+    height: 80,
+  },
+  text: {
+    fontSize: 16,
+  }
+});
